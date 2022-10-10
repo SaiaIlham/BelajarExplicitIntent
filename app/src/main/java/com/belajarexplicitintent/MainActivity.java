75e05dc2eb0e05f1ae5f655b33565e8cc1daef2f
SaiaIlham <ilhamindra09@mhs.mdp.ac.id>
2022-10-10 15:23:50 +0700

    lanjutan 2
    belom kelar

--- app/src/main/java/com/belajarexplicitintent/MainActivity.java
+++ app/src/main/java/com/belajarexplicitintent/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btnPindah;
+    EditText etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +18,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnPindah = findViewById(R.id.btn_pindah);
+        etNama = findViewById(R.id.et_nama);
+
         btnPindah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View View) {
+                String nama = etNama.getText().toString();
+
+                if (nama.trim().equals("")){
+                    etNama.setError("Nama Harus Diisi");
+
+
+                }
+                else{
+                    Intent pindah = 
+                }
+
                 Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(pindah);
             }
